2a52a2b57345e2079e9361b4d4d82f3a89966b12
Kate Volkova <57253605+katevolkova1991@users.noreply.github.com>
2019-11-17 19:30:03 +0300

    Add files via upload

--- volkova_kate_lesson_3_dz3.py
+++ volkova_kate_lesson_3_dz3.py
@@ -5,10 +5,9 @@ def my_func (number1, number2, number3):
     return n[0] + n[1]
 
 def sum_numbers_with_args (*args):
-    sum = 0
-    for el in args:
-        sum = sum+el
-    return sum
+    n = sorted(args)
+    n.reverse()
+    return sum(n[:2])
 
 print(my_func(1, 3, 2))
 print(sum_numbers_with_args(1, 3, 2, 5, 6))
